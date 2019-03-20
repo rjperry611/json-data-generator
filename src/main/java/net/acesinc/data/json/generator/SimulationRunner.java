ec04b0d60c6916ef306dc721c0bb6c053a4ba666
Perry, Robert <roperry@visa.com>
2019-03-20 10:31:36 -0600

    add the ability to do custom file names on the file logger

--- src/main/java/net/acesinc/data/json/generator/SimulationRunner.java
+++ src/main/java/net/acesinc/data/json/generator/SimulationRunner.java
@@ -8,13 +8,15 @@ package net.acesinc.data.json.generator;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import net.acesinc.data.json.generator.config.SimulationConfig;
-import net.acesinc.data.json.generator.config.WorkflowConfig;
-import net.acesinc.data.json.generator.config.JSONConfigReader;
-import net.acesinc.data.json.generator.log.EventLogger;
-import net.acesinc.data.json.generator.workflow.Workflow;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import net.acesinc.data.json.generator.config.JSONConfigReader;
+import net.acesinc.data.json.generator.config.SimulationConfig;
+import net.acesinc.data.json.generator.config.WorkflowConfig;
+import net.acesinc.data.json.generator.log.EventLogger;
+import net.acesinc.data.json.generator.workflow.Workflow;
 
 /**
  *
@@ -23,30 +25,32 @@ import org.apache.logging.log4j.Logger;
 public class SimulationRunner {
 
     private static final Logger log = LogManager.getLogger(SimulationRunner.class);
-    private SimulationConfig config;
-    private List<EventGenerator> eventGenerators;
-    private List<Thread> eventGenThreads;
+    private final SimulationConfig config;
+    private final List<EventGenerator> eventGenerators;
+    private final List<Thread> eventGenThreads;
     private boolean running;
-    private List<EventLogger> eventLoggers;
+    private final List<EventLogger> eventLoggers;
 
     public SimulationRunner(SimulationConfig config, List<EventLogger> loggers) {
         this.config = config;
         this.eventLoggers = loggers;
-        eventGenerators = new ArrayList<EventGenerator>();
-        eventGenThreads = new ArrayList<Thread>();
+        this.eventGenerators = new ArrayList<EventGenerator>();
+        this.eventGenThreads = new ArrayList<Thread>();
 
         setupSimulation();
     }
 
     private void setupSimulation() {
-        running = false;
-        for (WorkflowConfig workflowConfig : config.getWorkflows()) {
+        this.running = false;
+        for (WorkflowConfig workflowConfig : this.config.getWorkflows()) {
             try {
-                Workflow w = JSONConfigReader.readConfig(this.getClass().getClassLoader().getResourceAsStream(workflowConfig.getWorkflowFilename()), Workflow.class);
-                final EventGenerator gen = new EventGenerator(w, workflowConfig, eventLoggers);
-                log.info("Adding EventGenerator for [ " + workflowConfig.getWorkflowName()+ "," + workflowConfig.getWorkflowFilename()+ " ]");
-                eventGenerators.add(gen);
-                eventGenThreads.add(new Thread(gen));
+                Workflow w = JSONConfigReader.readConfig(
+                        this.getClass().getClassLoader().getResourceAsStream(workflowConfig.getWorkflowFilename()), Workflow.class);
+                final EventGenerator gen = new EventGenerator(w, workflowConfig, this.eventLoggers);
+                log.info("Adding EventGenerator for [ " + workflowConfig.getWorkflowName() + ","
+                        + workflowConfig.getWorkflowFilename() + " ]");
+                this.eventGenerators.add(gen);
+                this.eventGenThreads.add(new Thread(gen));
             } catch (IOException ex) {
                 log.error("Error reading config: " + workflowConfig.getWorkflowName(), ex);
             }
@@ -56,27 +60,36 @@ public class SimulationRunner {
     public void startSimulation() {
         log.info("Starting Simulation");
 
-        if (eventGenThreads.size() > 0) {
-            for (Thread t : eventGenThreads) {
+        if (this.eventGenThreads.size() > 0) {
+            for (Thread t : this.eventGenThreads) {
                 t.start();
             }
-            running = true;
+            this.running = true;
         }
     }
 
     public void stopSimulation() {
         log.info("Stopping Simulation");
-        for (Thread t : eventGenThreads) {
+        for (Thread t : this.eventGenThreads) {
             t.interrupt();
         }
-        for (EventLogger l : eventLoggers) {
+        for (EventLogger l : this.eventLoggers) {
             l.shutdown();
         }
-        running = false;
+        this.running = false;
     }
 
     public boolean isRunning() {
-        return running;
+        return this.running && anyGeneratorsRunning();
+    }
+
+    public boolean anyGeneratorsRunning() {
+        for(EventGenerator eventGenorator : eventGenerators) {
+            if(eventGenorator.isRunning()) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
--- src/main/java/net/acesinc/data/json/generator/log/FileLogger.java
+++ src/main/java/net/acesinc/data/json/generator/log/FileLogger.java
@@ -5,12 +5,21 @@
  */
 package net.acesinc.data.json.generator.log;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.pulsar.shade.org.apache.commons.lang3.StringUtils;
 
 /**
  *
@@ -22,10 +31,25 @@ public class FileLogger implements EventLogger {
     public static final String OUTPUT_DIRECTORY_PROP_NAME = "output.directory";
     public static final String FILE_PREFIX_PROP_NAME = "file.prefix";
     public static final String FILE_EXTENSION_PROP_NAME = "file.extension";
+    public static final String NUM_OF_LINES = "file.lines";
+    public static final String FILE_NAME_PATTERN_PROP_NAME = "file.pattern";
+    public static final String DATE_ROLLUP_PROP_NAME = "file.date.rollup";
 
     private File outputDirectory;
     private String filePrefix;
     private String fileExtension;
+    private String fileNamePattern;
+    private int numOfLines = -1;
+    private boolean genRandomName = false;
+    private boolean dateRollup = false;
+    private File f;
+    private BufferedWriter writer;
+
+    private int counterOfLines = 1;
+    private int totalCount = 1;
+    private int countForDate = 1;
+    private String lastDateAdjustedFileName = "";
+    private boolean firstEvent = true;
 
     public FileLogger(Map<String, Object> props) throws IOException {
         String outputDir = (String) props.get(OUTPUT_DIRECTORY_PROP_NAME);
@@ -37,28 +61,121 @@ public class FileLogger implements EventLogger {
                 }
             }
         }
+
+        fileNamePattern = (String) props.get(FILE_NAME_PATTERN_PROP_NAME);
+        if(StringUtils.isBlank(fileNamePattern)) {
+            genRandomName = true;
+        }
+
+
         filePrefix = (String) props.get(FILE_PREFIX_PROP_NAME);
+        filePrefix = filePrefix != null ? filePrefix : "";
+        if(genRandomName) {
+            filePrefix = StringUtils.rightPad(filePrefix, 3, '_');
+        }
+
         fileExtension = (String) props.get(FILE_EXTENSION_PROP_NAME);
+        fileExtension = fileExtension != null ? fileExtension : ".json";
+
+        String numOfLines = (String) props.get(NUM_OF_LINES);
+        if(numOfLines != null) {
+            this.numOfLines = Integer.valueOf(numOfLines);
+        }
+
+        Boolean dateRollup = (Boolean) props.get(DATE_ROLLUP_PROP_NAME);
+        if(dateRollup != null) {
+            this.dateRollup = dateRollup;
+        }
+
+        log.info("Running with: Number of lines: "+this.numOfLines);
     }
 
     @Override
-    public void logEvent(String event, Map<String, Object> producerConfig) {
+    public synchronized void logEvent(String event, Map<String, Object> producerConfig) {
         logEvent(event);
     }
-    
+
     private void logEvent(String event) {
         try {
-            File f = File.createTempFile(filePrefix, fileExtension, outputDirectory);
-            FileUtils.writeStringToFile(f, event, "UTF-8");
+            createFile();
+            writer.write(event+"\n");
+            counterOfLines++;
         } catch (IOException ioe) {
             log.error("Unable to create temp file");
         }
 
     }
 
+    private void createFile() throws IOException {
+        if(genRandomName) {
+            if(firstEvent || counterOfLines > numOfLines) {
+                if(writer != null) {
+                    writer.close();
+                }
+                f = File.createTempFile(filePrefix, fileExtension, outputDirectory);
+                writer = new BufferedWriter(new FileWriter(f));
+                counterOfLines = 1;
+                firstEvent = false;
+            }
+        } else {
+            String fileName = fileNamePattern;
+
+            // Set date fields
+            Pattern pattern = Pattern.compile("%t\\{(.+)\\}");
+            Matcher matcher = pattern.matcher(fileName);
+            Date now = new Date();
+            while(matcher.find()) {
+                DateFormat format = new SimpleDateFormat(matcher.group(1));
+                fileName = fileName.replace(matcher.group(), format.format(now));
+            }
+
+            boolean useNewPath = false;
+            if(firstEvent) {
+                lastDateAdjustedFileName = fileName;
+                useNewPath = true;
+                firstEvent = false;
+            } else {
+                if(dateRollup && !fileName.equals(lastDateAdjustedFileName)) {
+                    countForDate = 1;
+                    totalCount++;
+                    counterOfLines = 1;
+                    useNewPath = true;
+                    lastDateAdjustedFileName = fileName;
+                }
+
+                if(numOfLines > 0 && counterOfLines > numOfLines) {
+                    counterOfLines = 1;
+                    if(!useNewPath) {
+                        countForDate++;
+                        totalCount++;
+                        useNewPath = true;
+                    }
+                }
+            }
+
+            // Set file count
+            fileName = fileName.replaceAll("%c", ""+countForDate);
+            fileName = fileName.replaceAll("%C", ""+totalCount);
+
+            fileName = filePrefix + fileName + fileExtension;
+
+            if(useNewPath) {
+                if(writer != null) {
+                    writer.close();
+                }
+                f = new File(outputDirectory, fileName);
+                writer = new BufferedWriter(new FileWriter(f));
+            }
+        }
+    }
+
     @Override
     public void shutdown() {
-        //we don't need to shut anything down
+        try {
+            writer.close();
+        } catch (IOException e) {
+            log.error(e);
+        }
     }
 
 }
